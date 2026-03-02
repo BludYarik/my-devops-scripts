2210884a0ba1d1879a35c7733f1267cdd5ffdde7
yaroslav <asef535@gmail.com>
2026-03-03 01:24:16 +0200

    add

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+LOGFILE="/data/data/com.termux/files/home/second_devops/scripts/log.txt"
 SITES="google.com ukr.net facebook.com ne-isnue.com" 
 echo "--- Starting Check: $(date) ---"
 for SITE in $SITES
@@ -6,10 +7,10 @@ do
 STATUS=$(curl -Ls -o dev/null -w "%{http_code}" $SITE)
 
 if [ "$STATUS" == "200" ]; then
-	echo "[OK] $(date): $SITE is UP (Code: $STATUS)" >> log.txt
+	echo "[OK] $(date): $SITE is UP (Code: $STATUS)" >> $LOGFILE
 else
-	echo "[ERR] $(date): $SITE is DOWN (Code: $STATUS)" >> log.txt
+	echo "[ERR] $(date): $SITE is DOWN (Code: $STATUS)" >> $LOGFILE
 fi
 done
 
-echo "Check Done. Result in log,txt"
+echo "Check Done. Result in log.txt"
